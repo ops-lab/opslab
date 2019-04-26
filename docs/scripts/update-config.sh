68f8f9858a700aeb85a92e9e6323ad75b1ec630d
jiuchou <45912997+jiuchou@users.noreply.github.com>
2019-04-26 20:00:47 +0800

    Update update-config.sh

--- docs/scripts/update-config.sh
+++ docs/scripts/update-config.sh
@@ -3,10 +3,12 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-ROOT_PATH=$(cd $(dirname $0)/../..; pwd -P)
+ROOTPATH=$(cd $(dirname $0); pwd -P)
 
 HOST=x.x.x.x
 
-sed -i "s/127.0.0.1/${HOST}/g" ${ROOTPATH}/opslab/common/utils/insertPermissionToDB.sh
-sed -i "s/127.0.0.1/${HOST}/g" ${ROOTPATH}/opslab/opslab/settings.py
+sed -i "s/ALLOWED_HOSTS.*/ALLOWED_HOSTS = [\"${HOST}\"]/g" ${ROOTPATH}/opslab/opslab/settings.py
 sed -i "s/'NAME': 'autosolution'/'NAME': 'auto_solution'/g" ${ROOTPATH}/opslab/opslab/settings.py
+sed -i "s/127.0.0.1/${HOST}/g" ${ROOTPATH}/opslab/opslab/settings.py
+
+sed -i "s/127.0.0.1/${HOST}/g" ${ROOTPATH}/opslab/common/utils/insertPermissionToDB.sh
